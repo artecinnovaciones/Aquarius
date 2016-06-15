07520196ab8ae90202c5f70a9c27c3168ffdc5bf
artecinnovaciones <geovany150@hotmail.com>
2016-06-15 17:00:49 -0500

    vista main

---
+++ app/src/main/java/com/artecinnovaciones/aquarius/DetallesActivity.java
@@ -0,0 +1,48 @@
+package com.artecinnovaciones.aquarius;
+
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+
+public class DetallesActivity extends AppCompatActivity {
+
+    private static final String EXTRA_DRAWABLE = "com.artecinnovaciones.artecdemo.drawable";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalles);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarScroll);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        CollapsingToolbarLayout collapser =
+                (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        collapser.setTitle("Peces de agua salada");
+
+        loadImageParallax(R.drawable.peces_dulce);
+    }
+
+    private void loadImageParallax(int id) {
+        ImageView image = (ImageView) findViewById(R.id.image_paralax);
+        // Usando Glide para la carga asíncrona
+        Glide.with(this)
+                .load(id)
+                .centerCrop()
+                .into(image);
+    }
+}
