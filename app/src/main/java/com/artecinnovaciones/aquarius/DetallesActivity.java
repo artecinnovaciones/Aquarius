b5a5102e74dc64d18456d47395cdc28709bc253d
artecinnovaciones <geovany150@hotmail.com>
2016-06-22 19:24:29 -0500

    aaa

--- app/src/main/java/com/artecinnovaciones/aquarius/DetallesActivity.java
+++ app/src/main/java/com/artecinnovaciones/aquarius/DetallesActivity.java
@@ -3,8 +3,6 @@ package com.artecinnovaciones.aquarius;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,23 +10,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import android.widget.SearchView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import android.support.v4.view.MenuItemCompat.OnActionExpandListener;
-import android.support.v7.widget.SearchView.OnQueryTextListener;
 
 import com.artecinnovaciones.aquarius.fragments.DetallesFragment;
 import com.bumptech.glide.Glide;
 
-public class DetallesActivity extends AppCompatActivity implements OnQueryTextListener, OnActionExpandListener {
+public class DetallesActivity extends AppCompatActivity {
 
     private static final String EXTRA_DRAWABLE = "com.artecinnovaciones.artecdemo.drawable";
     String tip="";
-    TextView txtB;
-    RelativeLayout bus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +27,6 @@ public class DetallesActivity extends AppCompatActivity implements OnQueryTextLi
         setSupportActionBar(toolbar);
 
         tip=getIntent().getStringExtra("tipo");
-
-        txtB = (TextView) findViewById(R.id.text_buscar);
-        bus=(RelativeLayout) findViewById(R.id.layout_buscar);
 
         DetallesFragment DetFrag = new DetallesFragment(tip);
         getFragmentManager().beginTransaction()
@@ -52,8 +38,9 @@ public class DetallesActivity extends AppCompatActivity implements OnQueryTextLi
             public void onClick(View view) {
                // Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                  //       .setAction("Action", null).show();
-                bus.setVisibility(View.VISIBLE);
+                if (menu != null) menu.clear();
                 inflater.inflate(R.menu.menu_detalles, menu);
+                searchItem.expandActionView();
 
             }
         });
@@ -83,65 +70,18 @@ public class DetallesActivity extends AppCompatActivity implements OnQueryTextLi
        inflater = getMenuInflater();
        this.menu=menu;
 
-        MenuItem searchItem = menu.findItem(R.id.action_search);
-
-        android.support.v7.widget.SearchView searchView = (android.support.v7.widget.SearchView) MenuItemCompat.getActionView(searchItem);
-        searchView.setQueryHint("Buscar");
-        searchView.setOnQueryTextListener(this);
-
-        MenuItemCompat.setOnActionExpandListener(searchItem, this);
-      //  inflater.inflate(R.menu.menu_detalles, menu);
-/*
-        final MenuItem searchItem = menu.findItem(R.id.action_search);
-        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
-        //permite modificar el hint que el EditText muestra por defecto
-        //searchView.setQueryHint(getText(R.string.search));
-        searchView.setQueryHint("Buscar");
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Toast.makeText(DetallesActivity.this, "submitted", Toast.LENGTH_SHORT).show();
-                //se oculta el EditText
-                searchView.setQuery("", false);
-                searchView.setIconified(true);
-                return true;
-            }
-
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                //textView.setText(newText);
-                return true;
-            }
-        }); */
-
+        searchItem = menu.findItem(R.id.action_search);
+        searchItem.setIcon(R.drawable.buscar);
 
         return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public boolean onMenuItemActionExpand(MenuItem item) {
-        Toast.makeText(getApplicationContext(), "EXPAND", Toast.LENGTH_SHORT).show();
-        return true;
-    }
-
-    @Override
-    public boolean onMenuItemActionCollapse(MenuItem item) {
-        Toast.makeText(getApplicationContext(), "COLLAPSE", Toast.LENGTH_SHORT).show();
-        return true;
-    }
-
-    @Override
-    public boolean onQueryTextSubmit(String s) {
-        txtB.setText("Texto a buscar\n\n" + s);
-        return false;
-    }
-
-    @Override
-    public boolean onQueryTextChange(String s) {
-        txtB.setText("Escribiendo texto...\n\n" + s);
-        return false;
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     private MenuInflater inflater;
     private Menu menu;
+    MenuItem searchItem;
 }
