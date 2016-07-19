6af8d5cc52050ee2e60c992f74a6198a755791c2
artecinnovaciones <geovany150@hotmail.com>
2016-07-19 11:23:47 -0500

    splash

--- app/src/main/java/com/artecinnovaciones/aquarius/fragments/DetallesFragment.java
+++ app/src/main/java/com/artecinnovaciones/aquarius/fragments/DetallesFragment.java
@@ -10,6 +10,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 import com.artecinnovaciones.aquarius.MainActivity;
 import com.artecinnovaciones.aquarius.R;
@@ -32,6 +34,8 @@ public class DetallesFragment extends Fragment {
     RecyclerView recycler;
     CardView Cardagresivos;
 
+    Animation aparecer;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_detalles, container, false);
@@ -43,6 +47,8 @@ public class DetallesFragment extends Fragment {
     public void detalles(View view) {
         recycler=ViewUtil.findViewById(view,R.id.recycler_peces);
         Cardagresivos=ViewUtil.findViewById(view,R.id.card_agresivos);
+
+        aparecer= AnimationUtils.loadAnimation(this.getActivity().getApplicationContext(),R.anim.transparencia);
 
         if(MainActivity.tipo_pez.equals("salada") || MainActivity.tipo_pez.equals("dulce")){
             GridLayoutManager layoutRecycler = new GridLayoutManager(getActivity(),2);
@@ -95,6 +101,7 @@ public class DetallesFragment extends Fragment {
         Cardagresivos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                recycler.startAnimation(aparecer);
                 recycler.setVisibility(View.VISIBLE);
             }
         });
