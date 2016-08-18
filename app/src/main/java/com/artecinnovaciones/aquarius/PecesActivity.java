2111a1724f9b49f2666f4081d336e331494b8aa3
artecinnovaciones <geovany150@hotmail.com>
2016-08-18 11:41:28 -0500

    reacomodo7

--- app/src/main/java/com/artecinnovaciones/aquarius/PecesActivity.java
+++ app/src/main/java/com/artecinnovaciones/aquarius/PecesActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.widget.TextView;
 
 import com.artecinnovaciones.aquarius.adapters.ViewPagerAdapter;
 import com.artecinnovaciones.aquarius.fragments.PacificosFragment;
@@ -16,6 +18,10 @@ public class PecesActivity extends AppCompatActivity {
     TabLayout tabLayout;
     ViewPager viewPager;
     ViewPagerAdapter viewPagerAdapter;
+    private int[] tabIcons = {
+            R.drawable.ic_agresivo,
+            R.drawable.ic_pacifico
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,11 +31,29 @@ public class PecesActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         tabLayout=(TabLayout)findViewById(R.id.tabLayout);
         viewPager=(ViewPager)findViewById(R.id.viewPager);
+
         viewPagerAdapter=new ViewPagerAdapter(getSupportFragmentManager());
         viewPagerAdapter.addFragments(new AgresivosFragment(),"Agresivos");
         viewPagerAdapter.addFragments(new PacificosFragment(),"Pacíficos");
 
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
+
+        setupTabIcons();
+
+        tabLayout.setSelectedTabIndicatorColor(getResources().getColor(R.color.icons));
+        tabLayout.setTabTextColors(getResources().getColor(R.color.primary_light),getResources().getColor(R.color.icons));
+    }
+
+    private void setupTabIcons() {
+        TextView tabOne = (TextView) LayoutInflater.from(this).inflate(R.layout.item_tabs, null);
+        tabOne.setText("Pacíficos");
+        tabOne.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_pacifico, 0, 0);
+        tabLayout.getTabAt(0).setCustomView(tabOne);
+
+        TextView tabTwo = (TextView) LayoutInflater.from(this).inflate(R.layout.item_tabs, null);
+        tabTwo.setText("Agresivos");
+        tabTwo.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.ic_agresivo, 0, 0);
+        tabLayout.getTabAt(1).setCustomView(tabTwo);
     }
 }
