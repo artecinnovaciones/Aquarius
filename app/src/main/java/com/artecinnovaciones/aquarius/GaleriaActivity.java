bf61e92bb15806c8243f6dfcb2afe28cb5cc1b61
artecinnovaciones <geovany150@hotmail.com>
2016-09-15 15:24:05 -0500

    Peceras

--- app/src/main/java/com/artecinnovaciones/aquarius/GaleriaActivity.java
+++ app/src/main/java/com/artecinnovaciones/aquarius/GaleriaActivity.java
@@ -1,209 +1,116 @@
 package com.artecinnovaciones.aquarius;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.animation.AnimatorSet;
-import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Point;
-import android.graphics.Rect;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.animation.DecelerateInterpolator;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
 import android.widget.ImageView;
 
-public class GaleriaActivity extends FragmentActivity {
+public class GaleriaActivity extends Activity {
 
-    private Animator mCurrentAnimator;
+    public static int mSelected = 0;
+    private GridView gridview;
+    // Hacemos referencia a nuestras imagenes en miniatura de la carpeta de recursos
+    public static Integer[] mImagesIds = {
+            R.drawable.escondite, R.drawable.galeria,
+            R.drawable.ideas_decoracion, R.drawable.ideas_fondo,
+            R.drawable.ideas_iluminacion, R.drawable.escondite
+    };
 
-    /**
-     * The system "short" animation time duration, in milliseconds. This duration is ideal for
-     * subtle animations or animations that occur very frequently.
-     */
-    private int mShortAnimationDuration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_galeria);
+        setContentView(R.layout.images_gallery);
 
-        // Hook up clicks on the thumbnail views.
-
-        final View thumb1View = findViewById(R.id.thumb_button_1);
-        thumb1View.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                zoomImageFromThumb(thumb1View, R.drawable.peces_peceras);
-            }
-        });
-
-        final View thumb2View = findViewById(R.id.thumb_button_2);
-        thumb2View.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                zoomImageFromThumb(thumb2View, R.drawable.peces_salada);
-            }
-        });
-
-        // Retrieve and cache the system's default "short" animation time.
-        mShortAnimationDuration = getResources().getInteger(android.R.integer.config_shortAnimTime);
+        //Hacemos referncia nuestro GridView
+        gridview = (GridView) findViewById(R.id.gridview);
+        //Asignamos el ImageAdapter que hemos creado en esta misma clase al GridView
+        gridview.setAdapter(new ImageAdapter(this));
+        //Nos movemos al elemento seleccionado del GridView
+        gridview.setSelection(mSelected);
     }
-
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                // Navigate "up" the demo structure to the launchpad activity.
-                // See http://developer.android.com/design/patterns/navigation.html for more.
-                NavUtils.navigateUpTo(this, new Intent(this, MainActivity.class));
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    protected void onStart() {
+        super.onStart();
+        gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+        gridview.setSelection(mSelected);
     }
 
-    /**
-     * "Zooms" in a thumbnail view by assigning the high resolution image to a hidden "zoomed-in"
-     * image view and animating its bounds to fit the entire activity content area. More
-     * specifically:
-     *
-     * <ol>
-     *   <li>Assign the high-res image to the hidden "zoomed-in" (expanded) image view.</li>
-     *   <li>Calculate the starting and ending bounds for the expanded view.</li>
-     *   <li>Animate each of four positioning/sizing properties (X, Y, SCALE_X, SCALE_Y)
-     *       simultaneously, from the starting bounds to the ending bounds.</li>
-     *   <li>Zoom back out by running the reverse animation on click.</li>
-     * </ol>
-     *
-     * @param thumbView  The thumbnail view to zoom in.
-     * @param imageResId The high-resolution version of the image represented by the thumbnail.
-     */
-    private void zoomImageFromThumb(final View thumbView, int imageResId) {
-        // If there's an animation in progress, cancel it immediately and proceed with this one.
-        if (mCurrentAnimator != null) {
-            mCurrentAnimator.cancel();
+    //ImageAdapter para almacenar las imagemes en el GridView
+    public class ImageAdapter extends BaseAdapter {
+        private Context mContext;
+        public ImageAdapter(Context c) {
+            mContext = c;
+        }
+        public int getCount() {
+            return mImagesIds.length;
+        }
+        public Object getItem(int position) {
+            return null;
+        }
+        public long getItemId(int position) {
+            return 0;
         }
 
-        // Load the high-resolution "zoomed-in" image.
-        final ImageView expandedImageView = (ImageView) findViewById(R.id.expanded_image);
-        expandedImageView.setImageResource(imageResId);
-
-        // Calculate the starting and ending bounds for the zoomed-in image. This step
-        // involves lots of math. Yay, math.
-        final Rect startBounds = new Rect();
-        final Rect finalBounds = new Rect();
-        final Point globalOffset = new Point();
-
-        // The start bounds are the global visible rectangle of the thumbnail, and the
-        // final bounds are the global visible rectangle of the container view. Also
-        // set the container view's offset as the origin for the bounds, since that's
-        // the origin for the positioning animation properties (X, Y).
-        thumbView.getGlobalVisibleRect(startBounds);
-        findViewById(R.id.container).getGlobalVisibleRect(finalBounds, globalOffset);
-        startBounds.offset(-globalOffset.x, -globalOffset.y);
-        finalBounds.offset(-globalOffset.x, -globalOffset.y);
-
-        // Adjust the start bounds to be the same aspect ratio as the final bounds using the
-        // "center crop" technique. This prevents undesirable stretching during the animation.
-        // Also calculate the start scaling factor (the end scaling factor is always 1.0).
-        float startScale;
-        if ((float) finalBounds.width() / finalBounds.height()
-                > (float) startBounds.width() / startBounds.height()) {
-            // Extend start bounds horizontally
-            startScale = (float) startBounds.height() / finalBounds.height();
-            float startWidth = startScale * finalBounds.width();
-            float deltaWidth = (startWidth - startBounds.width()) / 2;
-            startBounds.left -= deltaWidth;
-            startBounds.right += deltaWidth;
-        } else {
-            // Extend start bounds vertically
-            startScale = (float) startBounds.width() / finalBounds.width();
-            float startHeight = startScale * finalBounds.height();
-            float deltaHeight = (startHeight - startBounds.height()) / 2;
-            startBounds.top -= deltaHeight;
-            startBounds.bottom += deltaHeight;
-        }
-
-        // Hide the thumbnail and show the zoomed-in view. When the animation begins,
-        // it will position the zoomed-in view in the place of the thumbnail.
-        thumbView.setAlpha(0f);
-        expandedImageView.setVisibility(View.VISIBLE);
-
-        // Set the pivot point for SCALE_X and SCALE_Y transformations to the top-left corner of
-        // the zoomed-in view (the default is the center of the view).
-        expandedImageView.setPivotX(0f);
-        expandedImageView.setPivotY(0f);
-
-        // Construct and run the parallel animation of the four translation and scale properties
-        // (X, Y, SCALE_X, and SCALE_Y).
-        AnimatorSet set = new AnimatorSet();
-        set
-                .play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left,
-                        finalBounds.left))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top,
-                        finalBounds.top))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, startScale, 1f))
-                .with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, startScale, 1f));
-        set.setDuration(mShortAnimationDuration);
-        set.setInterpolator(new DecelerateInterpolator());
-        set.addListener(new AnimatorListenerAdapter() {
-            @Override
-            public void onAnimationEnd(Animator animation) {
-                mCurrentAnimator = null;
-            }
-
-            @Override
-            public void onAnimationCancel(Animator animation) {
-                mCurrentAnimator = null;
-            }
-        });
-        set.start();
-        mCurrentAnimator = set;
-
-        // Upon clicking the zoomed-in image, it should zoom back down to the original bounds
-        // and show the thumbnail instead of the expanded image.
-        final float startScaleFinal = startScale;
-        expandedImageView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (mCurrentAnimator != null) {
-                    mCurrentAnimator.cancel();
+        // Creamos un ImageView por cada imagen miniatura referenciada en nuestro ImageAdapter
+        public View getView(final int position, View convertView, ViewGroup parent) {
+            ImageView imageView;
+            if (convertView == null) {  // if it's not recycled, initialize some attributes
+                imageView = new ImageView(mContext);
+                imageView.setLayoutParams(new GridView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, 130));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+                    imageView.setCropToPadding(true);
+                } else {
+                    imageView.setScaleType(ImageView.ScaleType.FIT_XY);
                 }
-
-                // Animate the four positioning/sizing properties in parallel, back to their
-                // original values.
-                AnimatorSet set = new AnimatorSet();
-                set
-                        .play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left))
-                        .with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView, View.SCALE_X, startScaleFinal))
-                        .with(ObjectAnimator
-                                .ofFloat(expandedImageView, View.SCALE_Y, startScaleFinal));
-                set.setDuration(mShortAnimationDuration);
-                set.setInterpolator(new DecelerateInterpolator());
-                set.addListener(new AnimatorListenerAdapter() {
-                    @Override
-                    public void onAnimationEnd(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-
-                    @Override
-                    public void onAnimationCancel(Animator animation) {
-                        thumbView.setAlpha(1f);
-                        expandedImageView.setVisibility(View.GONE);
-                        mCurrentAnimator = null;
-                    }
-                });
-                set.start();
-                mCurrentAnimator = set;
+                imageView.setPadding(2, 2, 2, 2);
+            } else {
+                imageView = (ImageView) convertView;
             }
-        });
+            //Asignamos a cada ImageView una imagen de nuestro "Array" de recursos
+            //utilizamos "setImageResource" ya que nuestras imagenes estan almacenadas en una
+            //carpeta de recursos en nuestro proyecto.
+            // "mImagesIds" es un Array de enteros, ya que almacena el Id del recurso, no la imagen en si.
+            imageView.setImageResource(mImagesIds[position]);
+
+            //En el evento click del ImageView obtenemos el indice de la imagen seleccionada
+            imageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    mSelected = (Integer) view.getTag();
+                    notifyDataSetChanged();
+
+                    String index = String.valueOf(position);
+                    Bundle extras = new Bundle();
+
+                    //Pasamos el indice de la imagen seleccionada a la activity que se encarga de mostrar la image Original
+                    //y hacer el "swipe"
+                    extras.putString("position", index);
+                    startActivity(new Intent(GaleriaActivity.this, SwipeImageActivity.class).putExtras(extras).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK));
+                }
+            });
+            try {
+                imageView.setTag(position);
+                //Ponemos un borde de color naranja a la imagen en miniatura seleccionada en el GrisView
+                if (position == mSelected) {
+                    imageView.setBackgroundColor(Color.parseColor("#ff6203"));
+                } else {
+                    imageView.setBackgroundColor(Color.TRANSPARENT);
+                }
+            } catch (Exception e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return imageView;
+        }
     }
 }
---
+++ app/src/main/java/com/artecinnovaciones/aquarius/SwipeImageActivity.java
@@ -0,0 +1,168 @@
+package com.artecinnovaciones.aquarius;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.artecinnovaciones.aquarius.GaleriaActivity;
+import com.artecinnovaciones.aquarius.R;
+
+/**
+ * Created by LAP-NIDIA on 15/09/2016.
+ */
+public class SwipeImageActivity extends Activity {
+
+    //Hacemos referencia a nuestras imagenes originales de la carpeta de recursos
+    public static Integer[] mImagesIds = {
+            R.drawable.escondite, R.drawable.galeria,
+            R.drawable.ideas_decoracion, R.drawable.ideas_fondo,
+            R.drawable.ideas_iluminacion, R.drawable.escondite
+    };
+
+    private String[] imagesDescriptions = {
+            "image1", "Image2", "image3", "Image4", "Image5", "Image6"
+    };
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.swipe_images_layout);
+
+        //Optenenos del GridView de la activity anterior el indice de la imagen que hemos
+        //seleccionado
+        String i = getIntent().getStringExtra("position");
+        //Lo convertimos a entero
+        int index = Integer.parseInt(i);
+
+        //Obtenemos la decripcion de la imagen seleccionada de una lista en la carpeta values\arrays.xml
+       // imagesDescriptions = getResources().getStringArray(R.array.images_descriptions);
+
+        SwipeImagePagerAdapter swipeNewsAdapter = new SwipeImagePagerAdapter();
+        ViewPager newsPager = (ViewPager) findViewById(R.id.swipe_pager);
+        newsPager.setAdapter(swipeNewsAdapter);
+        newsPager.setCurrentItem(index);
+
+        newsPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i2) {
+                GaleriaActivity.mSelected = i;
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+    }
+
+    //Creamos el SwipeImagePagerAdapter. Donde utilizaremos el layout "show_images"
+    //para cargar la imagen original y los textos de descripcion de la imagen
+    private class SwipeImagePagerAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            return GaleriaActivity.mImagesIds.length;
+        }
+        /**
+         * Create the page for the given position.  The adapter is responsible
+         * for adding the view to the container given here, although it only
+         * must ensure this is done by the time it returns from
+         * {@link #finishUpdate(android.view.ViewGroup)}.
+         *
+         * @param collection The containing View in which the page will be shown.
+         * @param position   The page position to be instantiated.
+         * @return Returns an Object representing the new page.  This does not
+         * need to be a View, but can be some other container of the page.
+         */
+        @Override
+        public Object instantiateItem(ViewGroup collection, int position) {
+
+            LayoutInflater inflater = (LayoutInflater) collection.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            //Refereciamos el show_images layout
+            View view = inflater.inflate(R.layout.show_image, null);
+
+            //Refereciamos el objeto ImageView del "show_images" layout
+            ImageView imageView = (ImageView) view.findViewById(R.id.gallery_image);
+
+            //Asignamos a cada ImageView una imagen de nuestro "Array" de recursos
+            //utilizamos "setImageResource" ya que nuestras imagenes estan almacenadas en una
+            //carpeta de recursos en nuestro proyecto.
+            // "mImagesIds" es un Array de enteros, ya que almacena el Id del recurso, no la imagen en si.
+            imageView.setImageResource(mImagesIds[position]);
+
+            //Refereciamos el objeto TextView del "show_images" layout, para colocar la descripcion de la imagen
+            TextView imageDescription = (TextView) view.findViewById(R.id.image_description);
+
+            //Asignamos el test descriptivo de la imagen al objecto TextView.
+            imageDescription.setText(imagesDescriptions[position].toString());
+
+            //Adicionamos el "view" que hemos creado con los objectos ImageView y TextView a la coleccion ViewGroup
+            collection.addView(view, 0);
+
+            return view;
+        }
+
+        /**
+         * Remove a page for the given position.  The adapter is responsible
+         * for removing the view from its container, although it only must ensure
+         * this is done by the time it returns from {@link #finishUpdate(android.view.ViewGroup)}.
+         *
+         * @param collection The containing View from which the page will be removed.
+         * @param position   The page position to be removed.
+         * @param view       The same object that was returned by
+         *                   {@link #instantiateItem(android.view.View, int)}.
+         */
+        @Override
+        public void destroyItem(ViewGroup collection, int position, Object view) {
+            collection.removeView((View) view);
+        }
+        /**
+         * Determines whether a page View is associated with a specific key object
+         * as returned by instantiateItem(ViewGroup, int). This method is required
+         * for a PagerAdapter to function properly.
+         *
+         * @param view   Page View to check for association with object
+         * @param object Object to check for association with view
+         * @return
+         */
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return (view == object);
+        }
+        /**
+         * Called when the a change in the shown pages has been completed.  At this
+         * point you must ensure that all of the pages have actually been added or
+         * removed from the container as appropriate.
+         *
+         * @param arg0 The containing View which is displaying this adapter's
+         *             page views.
+         */
+        @Override
+        public void finishUpdate(ViewGroup arg0) {
+        }
+
+        @Override
+        public void restoreState(Parcelable arg0, ClassLoader arg1) {
+        }
+        @Override
+        public Parcelable saveState() {
+            return null;
+        }
+        @Override
+        public void startUpdate(ViewGroup arg0) {
+        }
+    }
+}
