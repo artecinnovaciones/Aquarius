41e29e0be83b9565bb16b3f54cdb0b29812a2e37
artecinnovaciones <geovany150@hotmail.com>
2016-09-30 11:13:06 -0500

    Peceras

--- app/src/main/java/com/artecinnovaciones/aquarius/MainActivity.java
+++ app/src/main/java/com/artecinnovaciones/aquarius/MainActivity.java
@@ -1,40 +1,60 @@
 package com.artecinnovaciones.aquarius;
 
-import android.animation.Animator;
-import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
-import android.os.AsyncTask;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.animation.AnimationUtils;
-import android.view.animation.Interpolator;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.LinearLayout;
 
 import com.artecinnovaciones.aquarius.fragments.PrincipalFragment;
-import com.artecinnovaciones.aquarius.servicioretrofit.Controlador.PecesControlator;
-import com.artecinnovaciones.aquarius.servicioretrofit.modelresponse.PecesResponse;
 
 public class MainActivity extends AppCompatActivity {
 
     public static int Agresivosvisible=0;
+
+    int permissionCheck = ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+    private static final int MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+
+            } else {
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE);
+
+            }
+        }
+
         PrincipalFragment mainFrag = new PrincipalFragment();
         getFragmentManager().beginTransaction()
                 .add(R.id.frag_principal, mainFrag).commit();
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_WRITE_EXTERNAL_STORAGE: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
+                } else {
+
+                }
+                return;
+            }
+        }
+    }
 }
